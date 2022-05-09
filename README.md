a35f92c2368913040a4ddfc7bba8c6a7c9f67346
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-05-08 23:31:34 -0500

    final e-commerce-back end

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # E-commerce Back End Starter Code
+
+## Purpose
+
+A website build for e-commerce companies so that they could use the latesh technologies to compete with other e-commerce companies
+
+## Built With
+
+- HTML
+- CSS
+- Javascript
+- Node.js
+- Express.js
+- SQL
+- Sequelize
+  -ORM
+
+## Git hub link
+
+## Contribution
+
+Made by Kavya Mantena
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,58 @@
 const router = require('express').Router();
+const { padStart } = require('lodash');
 const { Product, Category, Tag, ProductTag } = require('../../models');
+const { post } = require('./tag-routes');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
+// find all products
   // be sure to include its associated Category and Tag data
+router.get('/', (req, res) => {
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
+
+// find a single product by its `id`
+  // be sure to include its associated Category and Tag data
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag, 
+        attributes: ['tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // create new product
@@ -89,8 +129,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+// delete one product by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id: req.params.id
+    }
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
+
+
